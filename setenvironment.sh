e5540a3f02508f29db34cbde65cbdeeef094f9ca
Nicholas Hinds <hindsn@gmail.com>
2013-02-14 21:10:12 +1300

    Change setenvironment.sh to work on linux

--- setenvironment.sh
+++ setenvironment.sh
@@ -6,28 +6,28 @@ COMMON="core license identity lib"
 for i in $COMMON; do
    if [[ ! -e "$CWD/chrome/$i" ]]; then
       echo "Creating junction for Chrome: $i"
-      junction $CWD/chrome/$i $CWD/$i
+      ln -s  $CWD/$i $CWD/chrome/$i
    fi
    if [[ ! -e "$CWD/missinge.safariextension/$i" ]]; then
       echo "Creating junction for Safari: $i"
-      junction $CWD/missinge.safariextension/$i $CWD/$i
+      ln -s $CWD/$i $CWD/missinge.safariextension/$i
    fi
    if [[ ! -e "$CWD/firefox/missinge/data/$i" ]]; then
       echo "Creating junction for Firefox: $i"
-      junction $CWD/firefox/missinge/data/$i $CWD/$i
+      ln -s $CWD/$i $CWD/firefox/missinge/data/$i
    fi
    if [[ ! -e "$CWD/opera/$i" ]]; then
       echo "Creating junction for Opera: $i"
-      junction $CWD/opera/$i $CWD/$i
+      ln -s $CWD/$i $CWD/opera/$i
    fi
 done
 
 if [[ ! -f "$CWD/firefox/missinge/lib/localizations.js" ]]; then
    echo "Creating hardlink for Firefox localization module"
-   fsutil hardlink create $CWD/firefox/missinge/lib/localizations.js $CWD/core/localizations.js
+   ln -s $CWD/core/localizations.js $CWD/firefox/missinge/lib/localizations.js
 fi
 
 if [[ ! -f "$CWD/firefox/missinge/lib/utils.js" ]]; then
    echo "Creating hardlink for Firefox utilities module"
-   fsutil hardlink create $CWD/firefox/missinge/lib/utils.js $CWD/core/utils.js
+   ln -s $CWD/core/utils.js $CWD/firefox/missinge/lib/utils.js
 fi
